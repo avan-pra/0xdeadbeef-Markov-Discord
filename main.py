84bcba25f0003aac0aedec324525910f82cd5971
avan-pra <avan-pra@users.noreply.github.com>
2022-07-06 13:57:07 +0200

    add debug info, did not work this morning

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import threading
 import time
 import numpy
+from datetime import datetime
 
 wsurl = "wss://steakoverflow.42paris.fr/sockjs/999/ow2ptczb/websocket"
 vmsg = '["{\\"msg\\":\\"connect\\",\\"version\\":\\"1\\",\\"support\\":[\\"1\\",\\"pre2\\",\\"pre1\\"]}"]'
@@ -32,6 +33,11 @@ async def on_message(message):
 				return
 		await message.channel.send('Error: could not find channel in the database')
 
+def get_current_time():
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	return '[' + str(current_time) + ']'
+
 def get_food_truck_state():
 	ws = WebSocket()
 	ws.connect(wsurl)
@@ -45,10 +51,11 @@ def get_food_truck_state():
 	return int(jtruck_state['fields']['history'][0]['state']) # retreive 1st state value
 
 def notify_channels():
+	print(get_current_time() + ' notifying ' + str(len(channel_list)) + ' channels')
 	slots = ["12h15", "12h30", "12h45", "13h00", "13h15", "13h30", "13h45"];
 	for channel in channel_list:
+		print(get_current_time() + ' notifying: ' + channel.name)
 		client.loop.create_task(channel.send('Le foodtruck est ouvert, ' + numpy.random.choice(slots, 1, p=[0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.1])[0] + ' vite !!! @everyone'))
-		pass
 
 def thread_ft():
 	food_truck_state = get_food_truck_state()
@@ -56,8 +63,10 @@ def thread_ft():
 		new_food_truck_state = get_food_truck_state()
 		# print(new_food_truck_state)
 		if new_food_truck_state != food_truck_state:
+			print(get_current_time() + ' Food truck has changed state')
 			food_truck_state = new_food_truck_state
 			if (food_truck_state == 0): # food truck is open
+				print(get_current_time() + ' Food truck is open, notifying')
 				notify_channels()
 
 		time.sleep(20)
@@ -65,4 +74,5 @@ def thread_ft():
 if __name__ == "__main__":
 	threading.Thread(target=thread_ft).start()
 	load_dotenv()
+	print(get_current_time() + ' thread started, launching bot')
 	client.run(os.getenv('discord_token'))
